5bfe65216813a2648b1ed6c7e7136348f11039f9
Exomi <omarovadil43944@gmail.com>
2025-12-09 17:17:33 +0500

    добавил перса пока без анимаций

--- Assets/Mini First Person Controller/Scripts/FirstPersonLook.cs
+++ Assets/Mini First Person Controller/Scripts/FirstPersonLook.cs
@@ -2,38 +2,27 @@
 
 public class FirstPersonLook : MonoBehaviour
 {
-    [SerializeField]
-    Transform character;
-    public float sensitivity = 2;
-    public float smoothing = 1.5f;
+    [SerializeField] Transform playerBody; // объект "Игрок"
+    public float sensitivity = 2f;
 
-    Vector2 velocity;
-    Vector2 frameVelocity;
-
-
-    void Reset()
-    {
-        // Get the character from the FirstPersonMovement in parents.
-        character = GetComponentInParent<FirstPersonMovement>().transform;
-    }
+    float xRotation = 0f;
 
     void Start()
     {
-        // Lock the mouse cursor to the game screen.
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
     {
-        // Get smooth velocity.
-        Vector2 mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
-        Vector2 rawFrameVelocity = Vector2.Scale(mouseDelta, Vector2.one * sensitivity);
-        frameVelocity = Vector2.Lerp(frameVelocity, rawFrameVelocity, 1 / smoothing);
-        velocity += frameVelocity;
-        velocity.y = Mathf.Clamp(velocity.y, -90, 90);
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity;
 
-        // Rotate camera up-down and controller left-right from velocity.
-        transform.localRotation = Quaternion.AngleAxis(-velocity.y, Vector3.right);
-        character.localRotation = Quaternion.AngleAxis(velocity.x, Vector3.up);
+        // Горизонтальный поворот тела
+        playerBody.Rotate(Vector3.up * mouseX);
+
+        // Вертикальный поворот камеры
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
     }
 }
